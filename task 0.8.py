894c46bc5c8444588eeb99540257ba38c63e0012
Faaiq2804 <91597827+Faaiq2804@users.noreply.github.com>
2022-01-25 14:43:02 +0200

    Update task 0.8.py
    corrections made

--- task 0.8.py
+++ task 0.8.py
@@ -1,5 +1,36 @@
-def foo():
-    print("hello")
-    return("blue")
+num=int(120)
 
-print(f"x={x}")
+def convert_to_time(num):
+
+    hour = num//60
+    return hour
+hour = convert_to_time(num)
+
+def Convert_to_time(num):
+    minute = num%60
+    return minute
+minute = Convert_to_time(num)
+if hour == 1 and minute == 1:
+
+    print(hour, "hour", minute, "minute")
+
+elif hour > 1 and minute == 1:
+
+    print(hour, "hours", minute, "minute")
+
+elif hour == 1 and minute > 1:
+
+    print(hour, "hour", minute, "minutes")
+
+elif hour > 1 and minute > 1:
+
+    print(hour, "hours", minute, "minutes")
+
+elif hour < 1 and minute> 1:
+    print(minute, "minutes")
+
+elif hour == 1 and minute <1:
+    print(hour, "hour")
+    
+elif hour > 1 and minute < 1:
+    print(hour, "hours")
