e8cd2aa106010eaf529a085935b7749e0323c3cb
Faaiq2804 <91597827+Faaiq2804@users.noreply.github.com>
2022-01-26 10:05:03 +0200

    Update task 0.5.py
    added return

--- task 0.5.py
+++ task 0.5.py
@@ -4,8 +4,8 @@ def area_of_triangle(x, y, z):
 
     area = (s*(s - x) * (s - y) * (s - z))**0.5
 
-    print(area)
+    return (area)
 
 
 
-area_of_triangle(16,20,30)
+print(area_of_triangle(16,20,30))
