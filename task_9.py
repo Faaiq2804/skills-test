54cd66ef75ecd14fa708eec149131c9a5a6ea648
Faaiq2804 <91597827+Faaiq2804@users.noreply.github.com>
2022-03-14 23:01:48 +0200

    Update and rename task 0.9.py to task_9.py

--- task_9.py
+++ task_9.py
@@ -2,11 +2,10 @@ def check_vowels(text):
   list=("a","e",'i',"o","u","A","E","I","O","U")
   word=""
   for char in text:
-
-        if char in  list:
-            if char!=word:
-              word = word+char+" "
+     if char in  list:
+        if char!=word:
+          word = word+char+" "
   return word
 text= set('phIlosophy')
 word=check_vowels(text).lower()
-print("vowels:"+str(word))
+print("vowels:"+str(word))d))
