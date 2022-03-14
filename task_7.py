8e8e379340022ecc227ac22c151922d64def7a47
Faaiq2804 <91597827+Faaiq2804@users.noreply.github.com>
2022-03-14 22:57:11 +0200

    Update and rename task 0.7.py to task_7.py

---
+++ task_7.py
@@ -0,0 +1,8 @@
+def convert_to_fahrenheit(temp_in_celsius):
+      return(temp_in_celsius*1.8)+32
+print(convert_to_fahrenheit(32))
+
+
+def convert_to_Celsius(temp_in_fahrenheit):
+     return (temp_in_fahrenheit - 32) * 0.556
+print(convert_to_Celsius(110))
